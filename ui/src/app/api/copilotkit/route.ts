a1b0a18baa9cebe35a72fa1f9d66dcefd90b71e9
Kaustubh Agrawal <kaustubhkagrawal@gmail.com>
2025-05-31 15:16:27 +0530

    main sync

--- ui/src/app/api/copilotkit/route.ts
+++ ui/src/app/api/copilotkit/route.ts
@@ -1,53 +1,34 @@
+import { NextRequest } from "next/server";
 import {
   CopilotRuntime,
-  OpenAIAdapter,
   copilotRuntimeNextJSAppRouterEndpoint,
+  ExperimentalEmptyAdapter,
   langGraphPlatformEndpoint,
-  copilotKitEndpoint,
 } from "@copilotkit/runtime";
-import OpenAI from "openai";
-import { NextRequest } from "next/server";
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-const llmAdapter = new OpenAIAdapter({ openai } as any);
-const langsmithApiKey = process.env.LANGSMITH_API_KEY as string;
+const serviceAdapter = new ExperimentalEmptyAdapter();
+
+const runtime = new CopilotRuntime({
+  remoteEndpoints: [
+    langGraphPlatformEndpoint({
+      deploymentUrl: process.env.LANGGRAPH_DEPLOYMENT_URL || "",
+      // langsmithApiKey: process.env.LANGSMITH_API_KEY || "", // only used in LangGraph Platform deployments
+      agents: [
+        {
+          name: process.env.NEXT_PUBLIC_COPILOTKIT_AGENT_NAME || "",
+          description:
+            process.env.NEXT_PUBLIC_COPILOTKIT_AGENT_DESCRIPTION ||
+            "A helpful LLM agent.",
+        },
+      ],
+    }),
+  ],
+});
 
 export const POST = async (req: NextRequest) => {
-  const searchParams = req.nextUrl.searchParams;
-  const deploymentUrl =
-    searchParams.get("lgcDeploymentUrl") || process.env.LGC_DEPLOYMENT_URL;
-
-  const isCrewAi = searchParams.get("coAgentsModel") === "crewai";
-
-  const remoteEndpoint =
-    deploymentUrl && !isCrewAi
-      ? langGraphPlatformEndpoint({
-          deploymentUrl,
-          langsmithApiKey,
-          agents: [
-            {
-              name: "research_agent",
-              description: "Research agent",
-            },
-            {
-              name: "research_agent_google_genai",
-              description: "Research agent",
-              assistantId: "9dc0ca3b-1aa6-547d-93f0-e21597d2011c",
-            },
-          ],
-        })
-      : copilotKitEndpoint({
-          url:
-            process.env.REMOTE_ACTION_URL || "http://localhost:8000/copilotkit",
-        });
-
-  const runtime = new CopilotRuntime({
-    remoteEndpoints: [remoteEndpoint],
-  });
-
   const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
     runtime,
-    serviceAdapter: llmAdapter,
+    serviceAdapter,
     endpoint: "/api/copilotkit",
   });
 
--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -17,21 +17,29 @@ export default function ModelSelectorWrapper() {
   );
 }
 
-function Home() {
-  const { agent, lgcDeploymentUrl } = useModelSelectorContext();
+const runtimeUrl = process.env.NEXT_PUBLIC_COPILOTKIT_RUNTIME_URL;
+// When using Copilot Cloud, all we need is the publicApiKey.
+const publicApiKey = process.env.NEXT_PUBLIC_COPILOT_API_KEY;
+// The name of the agent that we'll be using.
+const agentName = process.env.NEXT_PUBLIC_COPILOTKIT_AGENT_NAME
 
-  // This logic is implemented to demonstrate multi-agent frameworks in this demo project.
-  // There are cleaner ways to handle this in a production environment.
-  const runtimeUrl = lgcDeploymentUrl
-    ? `/api/copilotkit?lgcDeploymentUrl=${lgcDeploymentUrl}`
-    : `/api/copilotkit${
-        agent.includes("crewai") ? "?coAgentsModel=crewai" : ""
-      }`;
+function Home() {
+
+  // // This logic is implemented to demonstrate multi-agent frameworks in this demo project.
+  // // There are cleaner ways to handle this in a production environment.
+  // const runtimeUrl = lgcDeploymentUrl
+  //   ? `/api/copilotkit?lgcDeploymentUrl=${lgcDeploymentUrl}`
+  //   : `/api/copilotkit${
+  //       agent.includes("crewai") ? "?coAgentsModel=crewai" : ""
+  //     }`;
 
   return (
-    <CopilotKit runtimeUrl={runtimeUrl} showDevConsole={false} agent={agent}>
-      
+      <CopilotKit
+          runtimeUrl={runtimeUrl}
+          publicApiKey={publicApiKey}
+          agent={agentName}
+        >
       <Main />
-    </CopilotKit>
+        </CopilotKit>
   );
 }
